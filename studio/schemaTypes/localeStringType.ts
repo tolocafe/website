1b07154a339075094bc8fe2990c2a0781207df4d
Cesar Guadarrama <cesargdm@hey.com>
2025-12-19 14:12:52 -0600

    refactor: remove unnecessary whitespace in localeStringType and pageType schema files

--- studio/schemaTypes/localeStringType.ts
+++ studio/schemaTypes/localeStringType.ts
@@ -66,5 +66,3 @@ export const localeSlugType = defineType({
     fieldset: lang.isDefault ? undefined : 'translations',
   })),
 })
-
-
--- studio/schemaTypes/pageType.ts
+++ studio/schemaTypes/pageType.ts
@@ -76,5 +76,3 @@ export const pageType = defineType({
     },
   ],
 })
-
-
